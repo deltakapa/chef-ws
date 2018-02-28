f2c142b02a7ff52d39863ac3263052ded5e675cd
nO_x <dimiak_@xdev>
2018-03-01 01:22:40 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef-dev-repo
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/chef-dev-repo/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: chef-dev-repo
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
